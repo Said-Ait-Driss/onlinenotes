4bbf04651f48b39360d0143d4697b0e6a5eb7144
Said Ait Driss <71972356+Said-Ait-Driss@users.noreply.github.com>
2022-01-07 19:09:37 +0100

    add navbar and footer & main content

--- index.php
+++ index.php
@@ -13,8 +13,46 @@
 </head>
 
 <body>
-    <h1>online notes</h1>
-    <button class="btn btn-danger">my button</button>
+    <!-- * navbar -->
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+    <a class="navbar-brand" href="#">Online Notes</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb" aria-expanded="true">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div id="navb" class="navbar-collapse collapse hide">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Help</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact us</a>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#" data-target="#Loginmodal" data-toggle="modal"><span class="fas fa-log-in-alt"></span> Login</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+
+  <div class="container-fluid">
+    <div class="row sign_up_btn">
+      <p>Your notes with you wherever you go .</p>
+      <p>Ease to use .Protect all your notes.</p>
+      <div class="offset-md-3 col-md-4">
+        <button type="button" class="btn btn-success btn-lg" data-target="#modalmodal" data-toggle="modal">
+          Sign up
+        </button>
+      </div>
+    </div>
+  </div>
+
+
 </body>
 
 </html>
